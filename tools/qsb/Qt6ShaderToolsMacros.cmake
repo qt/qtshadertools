c0e2036ef7d9e7c261cdd9858f23232e27ad52cb
Lucie Gérard <lucie.gerard@qt.io>
2022-08-22 16:39:18 +0200

    Change the license of all CMakeLists.txt and *.cmake files to BSD
    Task-number: QTBUG-105718
    Change-Id: Iad2309c55ff46d23afd0c7e193989cd943b80e25
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    Reviewed-by: Jörg Bornemann <joerg.bornemann@qt.io>

--- tools/qsb/Qt6ShaderToolsMacros.cmake
+++ tools/qsb/Qt6ShaderToolsMacros.cmake
@@ -1,5 +1,5 @@
 # Copyright (C) 2022 The Qt Company Ltd.
-# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+# SPDX-License-Identifier: BSD-3-Clause
 
 # Invokes qsb on each file in FILES. Extensions must be .vert, .frag, or .comp.
 # The resulting .qsb files are added as resources under PREFIX.
